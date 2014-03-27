d3ba30f29b2353b3fc08b7c7d0add52b7fad21b1
Paul McLanahan <pmac@mozilla.com>
2014-03-27 17:03:53 -0400

    Add random ip generator and basic IP geo processing.

---
+++ bart_random.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Generate random IP addresses and insert them into redis.
+"""
+
+import argparse
+import random
+import socket
+import struct
+import sys
+import time
+
+import redis
+
+from smithers import conf
+
+
+parser = argparse.ArgumentParser(description='Bart throws random IPs at Lisa.')
+parser.add_argument('--count', default=250000, type=int,
+                    help='number of IPs')
+
+args = parser.parse_args()
+
+r = redis.StrictRedis()
+
+
+def get_random_ip():
+    # shamelessly stolen from the maxminddb benchmark script
+    # https://github.com/maxmind/MaxMind-DB-Reader-python/blob/master/examples/benchmark.py
+    return socket.inet_ntoa(struct.pack('!L', random.getrandbits(32)))
+
+
+for i in xrange(args.count):
+    r.lpush(conf.REDIS_BUCKETS['IPLOGS'], 'd:%s:%s' % (time.time(), get_random_ip()))
+    sys.stdout.write('.')
+    sys.stdout.flush()
---
+++ lisa.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""
+Grab IP addresses from Redis and lookup Geo info.
+"""
+
+import argparse
+import signal
+import sys
+
+import maxminddb
+from redis import StrictRedis
+
+from smithers import conf
+
+
+parser = argparse.ArgumentParser(description='Lisa does smart things with IPs.')
+parser.add_argument('--file', default='GeoIP2-City.mmdb',
+                    help='path to mmdb file')
+parser.add_argument('-v', '--verbose', action='store_true')
+
+
+args = parser.parse_args()
+redis = StrictRedis()
+
+try:
+    geo = maxminddb.Reader(args.file)
+except IOError:
+    print ('ERROR: Can\'t find MaxMind Database file (%s). '
+           'Try setting the --file flag.' % args.file)
+    sys.exit(1)
+
+
+while True:
+    ip_info = redis.brpop(conf.REDIS_BUCKETS['IPLOGS'])
+    rtype, rtime, ip = ip_info[1].split(':')
+    record = geo.get(ip)
+    if record:
+        redis.incr(conf.REDIS_BUCKETS['DOWNLOAD_TOTAL'])
+        country = record.get('country', record.get('registered_country'))
+        if country:
+            redis.hincrby(conf.REDIS_BUCKETS['DOWNLOAD_TOTAL_COUNTRY'],
+                      country['iso_code'], 1)
+
+    if args.verbose:
+        sys.stdout.write('.')
+        sys.stdout.flush()
