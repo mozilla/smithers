a640ab5761cc8dca8602f2045444d11f44340ec9
Paul McLanahan <pmac@mozilla.com>
2014-04-03 11:20:40 -0400

    Switch bart to gzip and only delete redis commands if redis-cli exists cleanly.

--- bart.sh
+++ bart.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
-# This needs to be cleaned up:
-# 1) if redis is down, it should queue up the commands rather than throw them away
-# 2) run from supervisord instead of cron
-# 3) proper temp file names (could conflict with #1)
-# 4) use proper logrotate system rather than this hack
-
 EPOCH=`date +%s`
 NEW_LOG="/var/log/glowmo/syslog/glow.${EPOCH}.log"
 
 mv -f /var/log/glow.log ${NEW_LOG}
 kill -HUP `cat /var/run/syslogd.pid`
 
-cat ${NEW_LOG} | grep -iE 'firefox-(28|latest)' | awk '{print "lpush geoip d," $2 "," $3}' > /tmp/redis-commands.txt
+cat ${NEW_LOG} | grep -iE 'firefox-(28|latest)' | awk '{print "lpush geoip d," $2 "," $3}' >> /tmp/redis-commands.txt
 
-cat /tmp/redis-commands.txt | redis-cli
+cat /tmp/redis-commands.txt | redis-cli && rm /tmp/redis-commands.txt
 
-rm /tmp/redis-commands.txt
-
-bzip2 ${NEW_LOG}
+gzip ${NEW_LOG}
